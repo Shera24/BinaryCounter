daa4656a6a504213c3952083fd4624d171215403
Shera24 <90753016+Shera24@users.noreply.github.com>
2022-06-12 21:59:46 +0200

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 # 2021 nr@bulme.at
+#Abgabe Lehner
 
-from gpiozero import Button
+from gpiozero import Button, LED
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLCDNumber, QApplication
 from PyQt5.QtCore import Qt, QObject, pyqtSignal
-from PyQt5.QtWidgets import (QWidget, QLCDNumber,
-    QVBoxLayout, QApplication)
+from threading import Thread
+import time
+
 
 DOWN_PIN = 22
 RESET_PIN = 27
-UP_PIN = 17 
+UP_PIN = 17
+
+leds = [LED(18),LED(23),LED(24),LED(25)]
+
+
+#led1=LED(18)
+#led2=LED(23)
+#led3=LED(24)
+#led4=LED(25)
 
 class QtButton(QObject):
     changed = pyqtSignal()
@@ -20,7 +32,8 @@ class QtButton(QObject):
 
     def gpioChange(self):
         self.changed.emit()
-
+        
+    
 class Counter(QWidget):
     def __init__(self):
         super().__init__()
@@ -38,17 +51,52 @@ class Counter(QWidget):
         self.setMinimumSize(400, 200)
         self.setWindowTitle('Counter')
         self.show()
+      
 
-
+    def defineLed (self, count):
+        for i in range(4):
+            div = 2 ** (3-i)
+            checkValue = int(count / div)
+            if checkValue % 2 == 1:
+                self.leds[i].on()
+            else:
+                self.leds[i].off()
+                
     def countUp(self):
-        self.count += 1
+        if self.count == 15:
+            self.count = 0
+            self.lcd.display(self.count)
+            self.defineLed(self.count)
+        else:
+            self.count += 1
+            self.lcd.display(self.count)
+            self.defineLed(self.count)
+            
+    def countDown(self):
+        if self.count == 0:
+            self.count = 15
+            self.lcd.display(self.count)
+            self.defineLed(self.count)
+        else:
+            self.count -= 1
+            self.lcd.display(self.count)
+            self.defineLed(self.count)
+    
+    def countReset(self):
+        self.count = 0
         self.lcd.display(self.count)
+        self.defineLed(self.count)
+        
  
 
 if __name__ ==  '__main__':
     app = QApplication([])
     gui = Counter()
-    button = QtButton(UP_PIN)
-    button.changed.connect(gui.countUp)
-    app.exec_()
+    buttonUp = QtButton(UP_PIN)
+    buttonUp.changed.connect(gui.countUp)
+    buttonDown = QtButton(DOWN_PIN)
+    buttonDown.changed.connect(gui.countDown)
+    buttonReset = QtButton(RESET_PIN)
+    buttonReset.changed.connect(gui.countReset)
     
+    app.exec_()
